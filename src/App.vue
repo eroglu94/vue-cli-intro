76395a9e0845fec27184eef47150e975c0a8f798
eroglu94 <eroglu1994@gmail.com>
2020-09-23 19:15:24 +0300

    firabase kullanımı

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,55 @@
 <template>
-  <div class="container-fluid text-center">
-    <br><br><br>
-    <h2>
-      <strong v-once :style="{'color' : getRandomColor()}">Poğaça </strong>
-      <strong v-once :style="{'color' : getRandomColor()}">Nerede </strong>
-      <strong v-once :style="{'color' : getRandomColor()}">?</strong>
-    </h2>
-    <br />
-    <p>
-      <strong
-        style="color: #555555"
-      >Açık kartlarda birini seçtikten sonra, kapalı olan karta tıklayınız</strong>
-    </p>
-
-    <div class="row d-flex justify-content-center" style="margin-top:50px">
-      <div class="col d-flex justify-content-center">
-        <div class="cats">
-          <img src="./assets/card-1.jpg" :class="{clicked : selectedCat == 1}" alt="" @click="selectCat(1,$event)">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Vue-Resource</h3>
+        <div class="form-btn-group">
+          <input type="text" class="form-control" v-model="userName" />
         </div>
-      </div>
-      <div class="col d-flex justify-content-center">
-        <div class="cats">
-          <img src="./assets/card-2.jpg" :class="{clicked : selectedCat == 2}" alt="" @click="selectCat(2,$event)">
-        </div>
-      </div>
-      <div class="col d-flex justify-content-center">
-        <div class="cats">
-          <img src="./assets/card-3.jpg" :class="{clicked : selectedCat == 3}" alt="" @click="selectCat(3,$event)">
-        </div>
-      </div>
-      <div class="col d-flex justify-content-center">
-        <div class="cats">
-          <img src="./assets/card-4.jpg" :class="{clicked : selectedCat == 4}" alt="" @click="selectCat(4,$event)">
-        </div>
-      </div>
-      <div class="col d-flex justify-content-center">
-        <div class="cats">
-          <img class="" src="./assets/card-5.jpg" :class="{clicked : selectedCat == 5}" alt="" @click="selectCat(5,$event)">
-        </div>
+        <button class="btn btn-primary" @click="saveUser()">Kaydet</button>
+        <hr />
+        <button class="btn btn-success" @click="getUsers()">Verileri Getir</button>
+        <hr />
+        <ul class="list-group">
+          <li class="list-group-item" v-for="user in userList">{{ user.userName }}</li>
+        </ul>
       </div>
     </div>
-
-<br>
-<br>
-
-<div class="row d-flex justify-content-center">
-  <div class="col d-flex justify-content-center">
-   <div class="cats">
-      <img src="./assets/logo.png" alt="" @click="revealRandomCat($event)">
-   </div>
-  </div>
-</div>
-
   </div>
 </template>
+
 <script>
 export default {
   data () {
     return {
-      selectedCat: 0,
-      randomCat: 0
+      userName: '',
+      userList: []
     }
-  },
-  filters: {
   },
   methods: {
-    getRandomColor () {
-      var letters = '0123456789ABCDEF'
-      var color = '#'
-      for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
-      }
-      return color
+    saveUser () {
+      this.$http.post('https://vuejs-vue-resource-7ecb9.firebaseio.com/users.json', { userName: this.userName })
+        .then((response) => {
+          console.log(response)
+        })
     },
-    selectCat (cat, $event) {
-      this.selectedCat = cat
-      $event.target.class.attach = 'clicked'
-    },
-    revealRandomCat ($event) {
-      $event.target.src = require('./assets/card-' + this.randomCat + '.jpg')
+    getUsers () {
+      this.$http.get('https://vuejs-vue-resource-7ecb9.firebaseio.com/users.json')
+        .then((response) => {
+          // console.log(response.data)
+          // console.log(response.data)
+          this.userList = []
+          const data = response.data
+          for (const key in data) {
+            console.log(data[key])
 
-      if (this.selectedCat === this.randomCat) {
-        console.log('you win')
-      } else {
-        console.log('you lose')
-      }
-
-      setTimeout(() => {
-        $event.target.src = require('./assets/logo.png')
-        this.selectedCat = 0
-      }, 1000)
+            this.userList.push(data[key])
+          }
+        })
     }
-
-  },
-  watch: {
-    selectedCat (value) {
-      this.randomCat = Math.floor(Math.random() * 5) + 1
-      console.log(this.randomCat)
-    }
-  },
-  computed: {
-    randomCatImgSrc () {
-      console.log('comp calıstı')
-      return require('./assets/card-' + this.randomCat + '.jpg')
-    }
-  },
-  created () {
-    this.randomCat = Math.floor(Math.random() * 5) + 1
   }
-
 }
 </script>
-<style type="sass">
-.cats {
-  /* background-color: rgb(219, 97, 97); */
-  width: 200px;
-  transform: scale(1,1);
-  transition: transform 0.2s;
-}
-.cats img{
-  border: 1px solid indianred;
-  transition: 0.2s;
-}
 
-.cats:hover{
-  /* box-shadow: 0px 0px 55px 0px   rgba(0,0,0,0.41); */
-  transform: scale(1.1,1.1);
-  transition: transform 0.2s;
-}
-
-.clicked{
-  box-shadow: 5px 5px 22px 0px   rgba(235, 8, 8, 0.41);
-  transform: scale(1.1,1.1);
-  transition: transform 0.2s;
-}
-
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 

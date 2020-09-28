5541756c6d8fb9d56111f3a5469de49db4738ece
eroglu94 <eroglu1994@gmail.com>
2020-09-28 19:36:46 +0300

    router geçişlerine animasyon eklemek

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,17 @@
     <div class="row">
       <div class="col-md-12">
         <hr>
-        <router-view name="header-top" ></router-view>
-        <router-view></router-view>
-        <router-view name="header-bottom" ></router-view>
+        <router-view name="header-top"></router-view>
+        <transition
+          name="slide"
+          mode="out-in"
+          type="animation"
+          enter-active-class="animate__animated animate__zoomIn animate__faster"
+          leave-active-class="animate__animated animate__zoomOut animate__faster"
+        >
+          <router-view></router-view>
+        </transition>
+        <router-view name="header-bottom"></router-view>
       </div>
     </div>
   </div>

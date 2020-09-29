f8bac157d9f6df0e2f77fd758fef38fe18d8f5ec
eroglu94 <eroglu1994@gmail.com>
2020-09-29 15:41:46 +0300

    Basit VUEX kullanımı

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-6 col-md-offset-3">
+        <h1 class="text-center">VUEX</h1>
         <hr>
-        <router-view name="header-top"></router-view>
-        <transition
-          name="slide"
-          mode="out-in"
-          type="animation"
-          enter-active-class="animate__animated animate__zoomIn animate__faster"
-          leave-active-class="animate__animated animate__zoomOut animate__faster"
-        >
-          <router-view></router-view>
-        </transition>
-        <router-view name="header-bottom"></router-view>
+        <app-result :counter="counter"></app-result>
+        <hr>
+        <app-counter @counterEvent="counter += $event"></app-counter>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-
+import Result from './components/Result'
+import Counter from './components/Counter'
 export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
   components: {
-    appHeader: Header
+    appResult: Result,
+    appCounter: Counter
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,34 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import VueRouter from 'vue-router'
-import { routes } from './routes'
-
-Vue.use(VueRouter)
-const routerA = new VueRouter({
-  routes: routes,
-  mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    if (to.hash) {
-      return {
-        selector: to.hash
-      }
-    }
-  }
-})
-
-routerA.beforeEach((to, from, next) => {
-  // next('/user')
-  // next({ name: 'anasayfa' })
-  // next(false)
-  // Global olarak bütün routeleri kontrol eder
-  console.log('Global olarak kontrol edildi.')
-  next()
-})
-
+import { store } from './store/store'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router: routerA
+  store: store
 }).$mount('#app')
